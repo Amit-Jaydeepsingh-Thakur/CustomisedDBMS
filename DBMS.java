d400f99f9c96bf12b46ff6cead14a4858e423b0d
Amit Jaydeepsingh Thakur <thakuramitjaydeep@gmail.com>
2022-05-29 18:37:54 +0530

    Update DBMS.java

--- DBMS.java
+++ DBMS.java
@@ -1,356 +1,281 @@
-
 import java.util.*;
 
-class Node
-{
- public int rno;
- public String name;
- public int age;
- public Node next;
+class Node {
+    public int rno;
+    public String name;
+    public int age;
+    public Node next;
 
- {
- this.next = null;
- }
+    {
+        this.next = null;
+    }
 
- public Node(int i,String str, int j)
- {
- this.rno = i;
- this.name = str;
- this.age = j;
- }
+    public Node(int i, String str, int j) {
+        this.rno = i;
+        this.name = str;
+        this.age = j;
+    }
 }
 
+interface LinkedLL {
+    public void InsertFirst(int i, String s, int j);
 
+    public void InsertLast(int i, String s, int j);
 
-interface LinkedLL
-{
- public void InsertFirst(int i,String s,int j);
+    public int Count();
 
- public void InsertLast(int i,String s,int j);
+    public void Display();
 
- public int Count();
+    public void Search(int z);
 
- public void Display();
+    public void Search(String y);
 
- public void Search(int z);
+    public void Delete(int a);
 
- public void Search(String y);
+    public void Delete(String a);
 
- public void Delete(int a);
-
- public void Delete(String a);
-
- public void Update(int a);
+    public void Update(int a);
 }
 
+class Student implements LinkedLL {
+    public Node head;
+    public int size;
 
-class Student implements LinkedLL
-{
- public Node head;
- public int size;
+    public Student() {
+        head = null;
+        size = 0;
+    }
 
- public Student()
- {
- head = null;
- size = 0;
- }
+    public void InsertFirst(int i, String s, int j) {
+        Node newn = new Node(i, s, j);
 
- public void InsertFirst(int i,String s,int j)
- {
- Node newn = new Node(i,s,j);
+        if (this.head == null) {
+            this.head = newn;
+        } else {
+            newn.next = this.head;
+            this.head = newn;
+        }
+        this.size++;
+    }
 
- if(this.head == null)
- {
- this.head = newn;
- }
- else
- {
- newn.next = this.head;
- this.head = newn;
- }
- this.size++;
- }
+    public void InsertLast(int i, String s, int j) {
+        Node temp = this.head;
+        Node newn = new Node(i, s, j);
 
- public void InsertLast(int i,String s,int j)
- {
- Node temp = this.head;
- Node newn = new Node(i,s,j);
+        if (this.head == null) {
+            this.head = newn;
+        } else {
+            while (temp.next != null) {
+                temp = temp.next;
+            }
 
- if(this.head == null)
- {
- this.head = newn;
- }
- else
- {
- while(temp.next != null)
- {
- temp = temp.next;
- }
+            temp.next = newn;
+        }
+        this.size++;
+    }
 
- temp.next = newn;
- }
- this.size++;
- }
+    public void Display() {
+    Node temp = this.head;
 
- public void Display()
- {
- Node temp = this.head;
+    System.out.println("Members of linked list");
 
- System.out.println("Members of linked list");
-
- while(temp != null)
- {
- System.out.println("Rno : "+temp.rno+" Name : "+temp.name+"
-:"+temp.age);
-
- temp = temp.next;
- }
+    while(temp != null) {
+        System.out.println("Rno : " + temp.rno + " Name : " + temp.name + ":" + temp.age);
+        temp = temp.next;
+    }
  }
 
- public int Count()
- {
- return this.size;
- }
+    public int Count() {
+        return this.size;
+    }
 
- public void Search(int no)
- {
- Node temp = this.head;
+    public void Search(int no) {
+        Node temp = this.head;
 
- while(temp != null)
- {
- if(temp.rno == no)
- {
- break;
- }
- temp = temp.next;
- }
+        while (temp != null) {
+            if (temp.rno == no) {
+                break;
+            }
+            temp = temp.next;
+        }
 
- if(temp == null)
- {
- System.out.println("There is no such member");
- }
- else
- {
- System.out.println("Member found of Roll no" +no);
- System.out.println("Name : "+temp.name);
- System.out.println("Roll number : "+temp.rno);
- System.out.println("Age : "+temp.age);
- }
- }
+        if (temp == null) {
+            System.out.println("There is no such member");
+        } else {
+            System.out.println("Member found of Roll no" + no);
+            System.out.println("Name : " + temp.name);
+            System.out.println("Roll number : " + temp.rno);
+            System.out.println("Age : " + temp.age);
+        }
+    }
 
+    public void Search(String str) {
+        Node temp = this.head;
 
- public void Search(String str)
- {
- Node temp = this.head;
+        while (temp != null) {
+            if (str.equals(temp.name)) {
+                break;
+            }
+            temp = temp.next;
+        }
 
- while(temp != null)
- {
- if(str.equals(temp.name))
- {
- break;
- }
- temp = temp.next;
- }
+        if (temp == null) {
+            System.out.println("There is no such member");
+        } else {
+            System.out.println("Member found of name " + str);
+            System.out.println("Name : " + temp.name);
+            System.out.println("Roll number : " + temp.rno);
+            System.out.println("Age : " + temp.age);
+        }
+    }
 
- if(temp == null)
- {
- System.out.println("There is no such member");
- }
- else
- {
- System.out.println("Member found of name " +str);
- System.out.println("Name : "+temp.name);
- System.out.println("Roll number : "+temp.rno);
- System.out.println("Age : "+temp.age);
- }
- }
+    public void Delete(int no) {
+        Node temp = this.head;
+        int i = 0;
+        if (head == null) {
+            return;
+        }
+        if (head.rno == no) {
+            head = head.next;
+            this.size--;
+            return;
+        }
 
- public void Delete(int no)
- {
- Node temp = this.head;
- int i = 0;
- if(head == null)
- {
- return;
- }
- if(head.rno == no)
- {
- head = head.next;
- this.size--;
- return;
- }
+        for (i = 1; i < this.size; i++) {
+            if (temp.next.rno == no) {
+                break;
+            }
+            temp = temp.next;
 
- for(i = 1; i< this.size;i++)
- {
- if(temp.next.rno == no)
- {
- break;
- }
- temp=temp.next;
+        }
 
- }
+        if (i == this.size) {
+            System.out.println("No element");
+        } else {
+            temp.next = temp.next.next;
+            this.size--;
+        }
+    }
 
- if(i == this.size)
- {
- System.out.println("No element");
- }
- else
- {
- temp.next=temp.next.next;
- this.size--;
- }
- }
+    public void Delete(String str) {
+        Node temp = this.head;
+        int i = 0;
+        if (head == null) {
+            return;
+        }
+        if (str.equals(head.name)) {
+            head = head.next;
+            this.size--;
+            return;
+        }
 
- public void Delete(String str)
- {
- Node temp = this.head;
- int i = 0;
- if(head == null)
- {
- return;
- }
- if(str.equals(head.name))
- {
- head = head.next;
- this.size--;
- return;
- }
+        for (i = 1; i < this.size; i++) {
+            if (str.equals(temp.next.name)) {
+                break;
+            }
+            temp = temp.next;
+        }
 
- for(i = 1; i< this.size;i++)
- {
- if(str.equals(temp.next.name))
- {
- break;
- }
- temp=temp.next;
- }
+        if (i == this.size) {
+            System.out.println("No element");
+        } else {
+            temp.next = temp.next.next;
+            this.size--;
+        }
+    }
 
- if(i == this.size)
- {
- System.out.println("No element");
- }
- else
- {
- temp.next=temp.next.next;
- this.size--;
- }
- }
+    public void Update(int no) {
+        Node temp = this.head;
 
+        while (temp != null) {
+            if (temp.rno == no) {
+                break;
+            }
+            temp = temp.next;
+        }
 
- public void Update(int no)
- {
- Node temp = this.head;
+        if (temp == null) {
+            System.out.println("There is no such member");
+        } else {
+            Scanner sobj = new Scanner(System.in);
+            System.out.println("Update : Name / Age / Both");
+            System.out.println("1/2/3");
 
- while(temp != null)
- {
- if(temp.rno == no)
- {
- break;
- }
- temp = temp.next;
- }
+            int option = sobj.nextInt();
+            System.out.println(option);
 
- if(temp == null)
- {
- System.out.println("There is no such member");
- }
- else
- {
- Scanner sobj = new Scanner(System.in);
- System.out.println("Update : Name / Age / Both");
- System.out.println("1/2/3");
+            switch (option) {
+                case 3:
+                case 1:
+                    System.out.println("Enter new name");
+                    sobj.nextLine();
+                    String s = sobj.nextLine();
+                    temp.name = s;
 
- int option = sobj.nextInt();
- System.out.println(option);
+                    if (option == 1)
+                        break;
 
- switch(option)
- {
- case 3:
- case 1:
- System.out.println("Enter new name");
- sobj.nextLine();
- String s = sobj.nextLine();
- temp.name = s;
+                case 2:
+                    System.out.println("Enter new age");
+                    int j = sobj.nextInt();
+                    temp.age = j;
+                    break;
+            }
 
- if(option == 1)
- break;
-
- case 2:
- System.out.println("Enter new age");
- int j = sobj.nextInt();
- temp.age = j;
- break;
- }
-
- }
- }
+        }
+    }
 }
 
+class customisedDBMS {
+    public static void main(String arg[]) {
+        Student ss = null;
 
-/////////////////////////////////////////////////
+        Scanner sobj = new Scanner(System.in);
 
-class DBMS
-{
- public static void main(String arg[])
- {
- Student ss = null;
+        System.out.println("DB Started");
 
- Scanner sobj = new Scanner(System.in);
+        while (true) {
+            System.out.println("Welcome to Amit's Customised DB:>>>>>");
+            String query = sobj.nextLine();
 
- System.out.println("DB Started");
+            String tokens[] = query.split("\\s");
 
- while(true)
- {
- System.out.println("Welcome to DB:>");
- String query = sobj.nextLine();
+            // stop
+            if (tokens.length == 1) {
+                if (query.equalsIgnoreCase("stop")) {
+                    System.out.println("Thank you for using Amit's customised DB");
+                    break;
+                }
+            }
 
- String tokens[] = query.split("\\s");
+            // select * from student;
+            if (tokens.length == 4) {
+                if (tokens[0].equalsIgnoreCase("select"))
+                    ss.Display();
+            }
 
- // stop
- if(tokens.length ==1)
- {
- if(query.equalsIgnoreCase("stop"))
- {
- System.out.println("Thank you for using   DB");
- break;
- }
- }
+            // insert into student value 11 as 21
+            // delete from student where rno = 11
+            if (tokens.length == 7) {
+                if (tokens[0].equalsIgnoreCase("insert")) {
 
- // select * from student;
- if(tokens.length == 4)
- {
- if(tokens[0].equalsIgnoreCase("select"))
- ss.Display();
- }
+                    ss.InsertFirst(Integer.parseInt(tokens[4]), tokens[5], Integer.parseInt(tokens[6]));
+                }
+                if (tokens[0].equalsIgnoreCase("delete")) {
+                    if (ss == null) {
+                        System.out.println("Table is not present");
+                        continue;
+                    }
 
- // insert into student value 11 asdas 21
- // delete from student where rno = 11
- if(tokens.length == 7)
- {
- if(tokens[0].equalsIgnoreCase("insert"))
- {
+                    ss.Delete(Integer.parseInt(tokens[6]));
+                }
+            }
 
-ss.InsertFirst(Integer.parseInt(tokens[4]),tokens[5],Integer.parseInt(tokens[6]));
- }
- if(tokens[0].equalsIgnoreCase("delete"))
-
- {
- if(ss == null)
- {
- System.out.println("Table is not present");
- continue;
- }
-
- ss.Delete(Integer.parseInt(tokens[6]));
- }
- }
-
- // create table student
- if(tokens.length == 3)
- {
- if(tokens[0].equalsIgnoreCase("create"))
- ss = new Student();
- }
- }
- }
+            // create table student
+            if (tokens.length == 3) {
+                if (tokens[0].equalsIgnoreCase("create"))
+                    ss = new Student();
+            }
+        }
+    }
 }
